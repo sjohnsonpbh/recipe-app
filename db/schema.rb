3c383a212aba58b1e7b3facedd3cbfce47853859
Scott Johnson <47424009+sjohnsonpbh@users.noreply.github.com>
2023-04-15 16:54:29 -0500

    add Dockerfile, add extensions to db, credentials.yml.enc

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_02_28_160421) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "timescaledb"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
