a95a3c35621a80db4641d7acd6d61c7ffbaca194
Scott Johnson <47424009+sjohnsonpbh@users.noreply.github.com>
2023-04-15 17:13:49 -0500

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,11 @@
 require "active_support/core_ext/integer/time"
 
+Rails.application.config.hosts << "recipe-app-production-6390.up.railway.app"
+
 Rails.application.configure do
   # error on railway - says to add the following in production
-  config.hosts << "recipe-app-production-6390.up.railway.app"
+  
+  
 
   # Settings specified here will take precedence over those in config/application.rb.
 
