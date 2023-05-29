42468cc7584d03ac99a225ec83537c14e7f33ce1
Scott Johnson <47424009+sjohnsonpbh@users.noreply.github.com>
2023-05-29 09:03:45 -0500

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
   end
 
   def current_user 
-    User.find(session[:user_id]) if session[:user_id]
+    User.find(session[:user_id]) if session[:user_id] 
   end
 
   def logged_in? 
