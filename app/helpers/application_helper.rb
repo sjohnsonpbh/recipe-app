6552fab90be0cc718dab833209993dcffa230b28
Scott Johnson <47424009+sjohnsonpbh@users.noreply.github.com>
2023-06-06 12:21:05 -0500

    replaced current_user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,11 +11,11 @@ module ApplicationHelper
   end
 
   def current_user 
-    # User.find(session[:user_id]) if session[:user_id] rescue ActiveRecord::RecordNotFound
+    User.find(session[:user_id]) if session[:user_id] rescue ActiveRecord::RecordNotFound
   end
 
   def logged_in? 
-    # !!current_user
+    !!current_user
   end
 
  
